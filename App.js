43876d011a3026166cf4abf053a3530556f13287
Hayan Alshoufi <hayanalshofi6@gmail.com>
2023-03-01 03:29:00 -0800

    splash screen

--- App.js
+++ App.js
@@ -1,34 +1,22 @@
 import {View, Text,Button} from 'react-native';
-import React from 'react';
+import React, { useEffect } from 'react';
 // import Counter from './src/components/Counter';
 import {Provider} from 'react-redux';
 import store from './src/redux/store';
 import Counter from './src/components/Counter';
-import "./i18n";
-import { useTranslation } from 'react-i18next';//add this line
-import RNRestart from "react-native-restart";
+import Test from './test'
+import SplashScreen from 'react-native-splash-screen'
+
 
 const App = () => {
-  const {t,i18n} = useTranslation();//add this line
-
+  useEffect(()=>{
+    setTimeout(()=>{SplashScreen.hide();},3000)
+    
+  });
   return (
     <View>
       <Provider store={store}>
-        <Text>{t('App')}</Text>
-        <Text>{t("dummy_text")}</Text>
-        <Button
-        title={t('changeLngBtn')}
-        onPress={() => {
-          i18n.changeLanguage(i18n.language === 'ar' ? 'en' : 'ar')
-          .then(() => {
-            I18nManager.forceRTL(i18n.language === 'ar');
-            RNRestart.Restart();
-          })
-          .catch(err => {
-            console.log('something went wrong while applying RTL');
-          });
-        }}
-      />
+        <Test/>
         <Counter/>
       </Provider>
     </View>
--- android/app/src/main/java/com/reactnativeredux/MainApplication.java
+++ android/app/src/main/java/com/reactnativeredux/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
 import java.util.List;
 import com.facebook.react.modules.i18nmanager.I18nUtil;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
 
 
 public class MainApplication extends Application implements ReactApplication {
@@ -22,11 +23,11 @@ public class MainApplication extends Application implements ReactApplication {
         }
 
         @Override
-        protected List<ReactPackage> getPackages() {
+        protected List<ReactPackage> getPackages() {   
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
+           packages.add(new SplashScreenReactPackage());
           return packages;
         }
 
